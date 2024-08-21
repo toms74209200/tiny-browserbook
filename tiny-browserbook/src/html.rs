5800ee7a91907056a38e66d279d0e51e69aea295
toms74209200 <motomotomato@gmail.com>
2024-08-21 15:53:44 +0900

    :sparkles: Add parser for open tag

--- tiny-browserbook/src/html.rs
+++ tiny-browserbook/src/html.rs
@@ -41,6 +41,21 @@ where
     .map(|attrs: Vec<(String, String)>| attrs.into_iter().collect::<AttrMap>())
 }
 
+fn open_tag<Input>() -> impl Parser<Input, Output = (String, AttrMap)>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    let open_tag_name = many1::<String, _, _>(letter());
+    let open_tag_content = (
+        open_tag_name,
+        many::<String, _, _>(space().or(newline())),
+        attributes(),
+    )
+        .map(|v: (String, _, AttrMap)| (v.0, v.2));
+    between(char('<'), char('>'), open_tag_content)
+}
+
 #[cfg(test)]
 mod tests {
     use combine::EasyParser;
@@ -77,5 +92,35 @@ mod tests {
     #[test]
     fn test_parse_non_attributes() {
         assert_eq!(attributes().easy_parse(""), Ok((AttrMap::new(), "")))
+    }
+
+    #[test]
+    fn test_parse_open_tag() {
+        assert_eq!(
+            open_tag().easy_parse("<p>aaaa"),
+            Ok((("p".to_string(), AttrMap::new()), "aaaa"))
+        );
+    }
+    #[test]
+    fn test_parse_open_tag_has_an_attribute() {
+        let mut attributes = AttrMap::new();
+        attributes.insert("id".to_string(), "test".to_string());
+        assert_eq!(
+            open_tag().easy_parse("<p id=\"test\">"),
+            Ok((("p".to_string(), attributes), ""))
+        )
+    }
+    #[test]
+    fn test_parse_open_tag_has_attributes() {
+        let result = open_tag().easy_parse("<p id=\"test\" class=\"sample\">");
+        let mut attributes = AttrMap::new();
+        attributes.insert("id".to_string(), "test".to_string());
+        attributes.insert("class".to_string(), "sample".to_string());
+        assert_eq!(result, Ok((("p".to_string(), attributes), "")));
+    }
+
+    #[test]
+    fn test_parse_open_tag_invalid() {
+        assert!(open_tag().easy_parse("<p id>").is_err());
     }
 }
