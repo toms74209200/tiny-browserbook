0b001743a92e309c538d0da214a774d20e13f3f9
toms74209200 <motomotomato@gmail.com>
2024-08-21 15:31:21 +0900

    :sparkles: Add parser for attributes

---
+++ tiny-browserbook/src/dom.rs
@@ -0,0 +1,3 @@
+use std::collections::HashMap;
+
+pub type AttrMap = HashMap<String, String>;
--- tiny-browserbook/src/html.rs
+++ tiny-browserbook/src/html.rs
@@ -1,3 +1,4 @@
+use crate::dom::AttrMap;
 use combine::between;
 use combine::error::ParseError;
 use combine::many;
@@ -6,6 +7,7 @@ use combine::parser::char::letter;
 use combine::parser::char::newline;
 use combine::parser::char::space;
 use combine::satisfy;
+use combine::sep_by;
 use combine::{many1, Parser, Stream};
 
 fn attribute<Input>() -> impl Parser<Input, Output = (String, String)>
@@ -27,8 +29,22 @@ where
         .map(|v| (v.0, v.4))
 }
 
+fn attributes<Input>() -> impl Parser<Input, Output = AttrMap>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    sep_by::<Vec<(String, String)>, _, _, _>(
+        attribute(),
+        many::<String, _, _>(space().or(newline())),
+    )
+    .map(|attrs: Vec<(String, String)>| attrs.into_iter().collect::<AttrMap>())
+}
+
 #[cfg(test)]
 mod tests {
+    use combine::EasyParser;
+
     use super::*;
 
     #[test]
@@ -45,5 +61,21 @@ mod tests {
             attribute().parse("test = \"foobar\""),
             Ok((("test".to_string(), "foobar".to_string()), ""))
         );
+    }
+
+    #[test]
+    fn test_parse_attributes() {
+        let mut expected_map = AttrMap::new();
+        expected_map.insert("test".to_string(), "foobar".to_string());
+        expected_map.insert("abc".to_string(), "def".to_string());
+        assert_eq!(
+            attributes().easy_parse("test=\"foobar\" abc=\"def\""),
+            Ok((expected_map, ""))
+        )
+    }
+
+    #[test]
+    fn test_parse_non_attributes() {
+        assert_eq!(attributes().easy_parse(""), Ok((AttrMap::new(), "")))
     }
 }
