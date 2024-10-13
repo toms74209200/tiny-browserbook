e4831967ba63522a7dd3592c1cd7f6ed3843da27
toms74209200 <motomotomato@gmail.com>
2024-10-13 20:50:51 +0900

    :sparkles: Add CSS rule

--- tiny-browserbook/src/css/css.rs
+++ tiny-browserbook/src/css/css.rs
@@ -17,6 +17,12 @@ pub enum CSSValue {
     Keyword(String),
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Rule {
+    pub selectors: Vec<Selector>,
+    pub declarations: Vec<Declaration>,
+}
+
 pub type Selector = SimpleSelector;
 
 #[derive(Debug, PartialEq)]
@@ -48,6 +54,23 @@ where
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
     many::<String, _, _>(space().or(newline()))
+}
+
+fn rule<Input>() -> impl Parser<Input, Output = Rule>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    (
+        selectors().skip(whitespaces()),
+        char::char('{').skip(whitespaces()),
+        declarations().skip(whitespaces()),
+        char::char('}'),
+    )
+        .map(|(selectors, _, declarations, _)| Rule {
+            selectors,
+            declarations,
+        })
 }
 
 fn selectors<Input>() -> impl Parser<Input, Output = Vec<Selector>>
@@ -145,6 +168,80 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_rule() {
+        assert_eq!(
+            rule().parse("test [foo=bar] {}"),
+            Ok((
+                Rule {
+                    selectors: vec![SimpleSelector::AttributeSelector {
+                        tag_name: "test".to_string(),
+                        attribute: "foo".to_string(),
+                        op: AttributeSelectorOp::Eq,
+                        value: "bar".to_string()
+                    }],
+                    declarations: vec![]
+                },
+                ""
+            ))
+        );
+    }
+
+    #[test]
+    fn test_rule_multiple() {
+        assert_eq!(
+            rule().parse("test [foo=bar], testtest[piyo~=guoo] {}"),
+            Ok((
+                Rule {
+                    selectors: vec![
+                        SimpleSelector::AttributeSelector {
+                            tag_name: "test".to_string(),
+                            attribute: "foo".to_string(),
+                            op: AttributeSelectorOp::Eq,
+                            value: "bar".to_string()
+                        },
+                        SimpleSelector::AttributeSelector {
+                            tag_name: "testtest".to_string(),
+                            attribute: "piyo".to_string(),
+                            op: AttributeSelectorOp::Contain,
+                            value: "guoo".to_string()
+                        }
+                    ],
+                    declarations: vec![]
+                },
+                ""
+            ))
+        );
+    }
+
+    #[test]
+    fn test_rule_value() {
+        assert_eq!(
+            rule().parse("test [foo=bar] { aa: bb; cc: dd; }"),
+            Ok((
+                Rule {
+                    selectors: vec![SimpleSelector::AttributeSelector {
+                        tag_name: "test".to_string(),
+                        attribute: "foo".to_string(),
+                        op: AttributeSelectorOp::Eq,
+                        value: "bar".to_string()
+                    }],
+                    declarations: vec![
+                        Declaration {
+                            name: "aa".to_string(),
+                            value: CSSValue::Keyword("bb".to_string())
+                        },
+                        Declaration {
+                            name: "cc".to_string(),
+                            value: CSSValue::Keyword("dd".to_string()),
+                        }
+                    ]
+                },
+                ""
+            ))
+        );
+    }
 
     #[test]
     fn test_selectors() {
